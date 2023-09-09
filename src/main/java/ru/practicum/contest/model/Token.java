02c14876f39edc4ec4708dede6d4dfa1713aa3d2
vvbakhanovich <vv.bakh@yandex.ru>
2023-09-09 18:11:12 +0300

    refactor: Добавить поля в Token

--- src/main/java/ru/practicum/contest/model/Token.java
+++ src/main/java/ru/practicum/contest/model/Token.java
@@ -1,4 +1,16 @@
 package ru.practicum.contest.model;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Token {
+    private String name;
+
+    private String token;
+
+    private String nextTaskUrl;
 }
