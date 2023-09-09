3bb0bc6c5891a6dd9cf121f85717f34b246b239c
vvbakhanovich <vv.bakh@yandex.ru>
2023-09-09 18:13:16 +0300

    feature: Реализовать методы toDto и toModel

--- src/main/java/ru/practicum/contest/mapper/TeamResponseMapper.java
+++ src/main/java/ru/practicum/contest/mapper/TeamResponseMapper.java
@@ -1,4 +1,24 @@
 package ru.practicum.contest.mapper;
 
+import org.mapstruct.Mapper;
+import ru.practicum.contest.dto.TokenDto;
+import ru.practicum.contest.model.Token;
+
+@Mapper
 public class TeamResponseMapper {
+    public TokenDto toDto(Token token) {
+        if (token == null) {
+            return null;
+        }
+
+        return new TokenDto(token.getName(), token.getToken(), token.getNextTaskUrl());
+    }
+
+    public Token toModel(TokenDto tokenDto) {
+        if (tokenDto == null) {
+            return null;
+        }
+
+        return new Token(tokenDto.getName(), tokenDto.getToken(), tokenDto.getNextTaskUrl());
+    }
 }
